e44656b4178bab6645f4e20286b6e3ab39cb98da
JuhGuilhermino <juh.guilhermino03@gmail.com>
2025-05-08 21:20:46 -0300

    base do projeto

--- src/core/Entrada.java
+++
@@ -1,7 +0,0 @@
-package core;
-
-public abstract class Entrada {
-    // TODO: Definir atributos comuns: autor, título, path
-    // TODO: Criar construtor e métodos getters
-    // TODO: Declarar método abstrato void exibir();
-}
