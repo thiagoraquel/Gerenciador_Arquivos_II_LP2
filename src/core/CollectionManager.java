d49a04427179dff8f8a272d80cbae62f04538bed
thiagoraquel <thiaguinho2005@yahoo.com.br>
2025-06-12 07:55:58 -0300

    Arquivo .bib sendo criado

--- src/core/CollectionManager.java
+++ src/core/CollectionManager.java
@@ -3,26 +3,11 @@ package core;
 import java.util.ArrayList;
 import java.util.List;
 import java.lang.String;
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
-import java.lang.String;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.Vector;
-import com.google.gson.Gson;
 import java.io.File;
-import java.io.PrintWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class CollectionManager {
     Directory directory;    // Usaremos o directory em questão para acessar
@@ -41,10 +26,13 @@ public class CollectionManager {
         Collection nova = new Collection(type, author, name, max_entrys, entrys);
         colecoes.add(nova);
         System.out.println("Coleção \"" + name + "\" criada com sucesso com " + entrys.size() + " entrada(s).");      
-        saveAsBib(nova, path);
+        
+        String outputPath = Paths.get(path, name + ".bib").toString();
+        saveAsBib(nova, outputPath);
     }
 
     public void saveAsBib(Collection collection, String outputPath) {
+        System.out.println("Salvando em: " + outputPath);
         try {
             File file = new File(outputPath);
             PrintWriter writer = new PrintWriter(file);
